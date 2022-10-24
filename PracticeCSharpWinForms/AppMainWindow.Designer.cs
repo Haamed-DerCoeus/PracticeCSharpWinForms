09d2b55f68ea62321a06cd0ffe7fc09ec2e31d92
Hamed <hamed.official2019@gmail.com>
2022-10-24 21:48:17 +0330

    main form(win) customized

--- PracticeCSharpWinForms/AppMainWindow.Designer.cs
+++ PracticeCSharpWinForms/AppMainWindow.Designer.cs
@@ -30,13 +30,14 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // AppMainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(737, 414);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "AppMainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Window";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
